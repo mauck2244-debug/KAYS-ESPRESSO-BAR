a5fd10a811f6e03e65f124d3fb5b9f9040e27bda
mauck2244-debug <mauck2244@gmail.com>
2025-12-04 12:34:40 +0700

    backup

---
+++ script.js
@@ -0,0 +1,31 @@
+// let length = 200;
+// let weight = 100;
+
+// let car = "Nissan";
+// let name = "John doe";
+
+// let isTrue = true;
+// let isFalse = false;
+
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 30
+// };
+
+// const cars = ["mama", "papa", "sister", "brother"];
+
+// let x;
+// console.log(typeof x);
+
+// let bigint = ("1234567890123456789012345678901234567890");
+
+// const sym1 = Symbol("Symbol1");
+// const sym2 = Symbol("Symbol2");
+// console.log(sym1 === sym2 );
+
+// let age = null;
+
+<button className="hamburger-icon" onClick={toggleMenu}>
+    ☰
+</button>
